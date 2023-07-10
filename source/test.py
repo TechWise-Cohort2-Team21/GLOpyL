d7e673216481f2c12a6519a29a0a1d29a94890a7
LanielleP <nellysbookshelf@gmail.com>
2023-07-09 19:23:26 -0700

    simplified logic and improved flexibility of input processing methods

--- source/test.py
+++ source/test.py
@@ -1,49 +1,60 @@
-#!pip3 install googletrans==3.1.0a0
 
+# todos:
+#[ ] INCLUDE TYPES IN KEYWORDS
+#[ ] deal with lines that include comments?
+#[ ] find better spanish translation for "else" and "elif"
+
+
+#pip3 install googletrans==3.1.0a0
 from googletrans import Translator, constants
 from pprint import pprint
 
-translator = Translator()
 
-# translate a spanish text to english text (by default)
-#translation = translator.translate("Hola Mundo")
+translated_language = "es"
+programming_language = "python"
+translator = Translator()
 #print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 
-keyword_file = open("keywords_en2es.txt", "r") #add logic here to select keyword file based on language
+
+keyword_file = open("keywords_en2"+translated_language+".txt", "r")
 keywords = {}
 for line in keyword_file:
     old, trans = line.strip().split(" ")
     keywords[old] = trans  
 keyword_file.close()
 
+
 def translate_word(word: str) -> str:
     if word in keywords:
         return keywords[word]
-    #add logic to choose joining method based on programming language
-    words = word.split("_")
-    return "_".join([translator.translate(s, dest="es").text for s in words])
+    if programming_language == "python":
+        words = word.split("_")
+        return translator.translate(" ".join(words), dest=translated_language).text.replace(" ", "_")
+
 
 def translate_line(line: str) -> str:
-    #todo: deal with lines that include "#"
-    items = []
+    words = []
     current_word = ""
-    for char in line:
-        if not char.isalpha() and char!="_":
-            if current_word!="":
-                items.append(translate_word(current_word))
-                current_word = ""
-            items.append(char)
-        else:
-            current_word += char
-    if current_word != "":
-        items.append(translate_word(current_word))
-    return "".join(items)
 
-english = open("input.txt", "r")
-with open("output.txt", "w") as spanish:
-    for line in english:
+    for char in line:
+        if char.isalpha() or char == "_":
+            current_word += char
+        elif current_word == "":
+            words.append(char)
+        else:
+            words.append(translate_word(current_word))
+            current_word = ""
+            words.append(char)
+
+    words.append(translate_word(current_word))
+    return "".join(words)
+
+
+input = open("input.txt", "r")
+with open("output.txt", "w") as output:
+    for line in input:
         translation = translate_line(line)
-        spanish.write(translation)
-        print(translation)
-english.close()
-spanish.close()
+        output.write(translation)
+        print(translation[:-1])
+input.close()
+output.close()
