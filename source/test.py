7cf3e2f265c0983734f9d7202b60431350f489c1
Defyinglight <Keshawn549@gmail.com>
2023-07-17 20:22:09 -0400

    Revert "updated comment for the translate line function and also added a way to remove comments from the code in the input.txt file."
    This reverts commit ee1954b0971bbb85a285cd7318010f0d9a21562a.

--- source/test.py
+++ source/test.py
@@ -1,17 +1,17 @@
+# original code. pre comments removal.
 from googletrans import Translator, constants
 from pprint import pprint
 
-# Setup: select languages and create translator object
+#Setup: select languages and create translator object
 translated_language = "es"
 programming_language = "python"
 translator = Translator()
 
-# Example of how to format/dissect output from a translation
-# print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+#Example of how to format/dissect output from a translation
+#print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 
-user_choice = input("Would you like to include comments? Type 'y' or 'n': ")
 
-# Keyword Dictionary: attempts to store correct keyword file in a dictionary
+#Keyword Dictionary: attempts to store correct keyword file in a dictionary
 keyword_file_path = f"keywords_en2{translated_language}.txt"
 try:
     with open(keyword_file_path, "r") as keyword_file:
@@ -23,11 +23,11 @@ except FileNotFoundError:
     print(f"Keyword file '{keyword_file_path}' not found.")
     exit(1)
 except Exception as e:
-    print(f"Error occurred while reading the keyword file: {(e)}")
+    print(f"Error occurred while reading the keyword file: {str(e)}")
     exit(1)
 
 
-# Translates individual keyword or variable/function name
+#Translates individual keyword or variable/function name
 def translate_word(word: str) -> str:
     if word in keywords:
         return keywords[word]
@@ -36,11 +36,10 @@ def translate_word(word: str) -> str:
         return translator.translate(" ".join(words), dest=translated_language).text.replace(" ", "_")
 
 
-# Translates a line, ignoring all nonalphabetical characters and optionally removes comments
-def translate_line(line: str, include_comments: bool = True) -> str:
-    if not include_comments and line.startswith("#"):
+#Translates a line, ignoring all nonalphabetical characters and removes comments
+def translate_line(line: str) -> str:
+    if line.startswith("#"):  # Skip lines that start with a '#'
         return ""
-
     words = []
     current_word = ""
 
@@ -56,21 +55,20 @@ def translate_line(line: str, include_comments: bool = True) -> str:
 
     words.append(translate_word(current_word))
     translated_line = "".join(words)
-    return translated_line
+    return "".join(words)
 
-
-# Applies translate functions to input file and also handles any errors that may appear
+#Applies translate functions to input file and also handles any errors that may appear
 input_file_path = "input.txt"
 output_file_path = "output.txt"
 
 try:
     with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
         for line in input_file:
-            translation = translate_line(line, include_comments=(user_choice.lower() == "y"))
+            translation = translate_line(line)
             output_file.write(translation)
 except FileNotFoundError:
     print(f"Input file '{input_file_path}' not found.")
 except Exception as e:
-    print(f"An error occurred during file operations: {(e)}")
+    print(f"An error occurred during file operations: {str(e)}")
 except Exception as e:
-    print(f"An unexpected error occurred: {(e)}")
+    print(f"An unexpected error occurred: {str(e)}")
