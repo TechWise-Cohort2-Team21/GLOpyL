1e8052717f134aed965d4fad1dd24748a0dd630e
Samueldnvn <116571481+Samueldnvn@users.noreply.github.com>
2023-07-20 12:58:13 -0400

    added drop down menu for languages

--- source/tkprototype.py
+++ source/tkprototype.py
@@ -1,11 +1,18 @@
 # Prototype Tkinter GUI
 import tkinter as tk
+from tkinter import ttk
 import test
 
 # Creates the window
 window = tk.Tk()
 window.title("GLOpyL")
 window.geometry("900x500")
+
+supported_languages = [
+    "Spanish",
+    "French"
+]
+
 
 # Translates the input box, places in output box
 def translateClick():
@@ -17,15 +24,26 @@ def translateClick():
         output.join(translation)
     outputTextBox.insert(output)
 
+def comboclick(event):
+    if language_selection.get() == "Spanish":
+        test.translated_language = "es"
+    elif language_selection.get() == "French":
+        test.translated_language = "fr"
+
+
+language_selection = ttk.Combobox(window, value=supported_languages, font=("Arial", 15), width=10)
+language_selection.current(0)
+language_selection.bind("<<ComboboxSelected>>", comboclick)
+language_selection.grid(column=2, row=1, padx=10, pady=10)
 
 inputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
-inputTextBox.grid(column=1, row=1, padx=10, pady=10)
+inputTextBox.grid(column=1, row=2, padx=10, pady=10)
 
 outputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
-outputTextBox.grid(column=3, row=1, padx=10, pady=10)
+outputTextBox.grid(column=3, row=2, padx=10, pady=10)
 
-translateButton = tk.Button(window, text="Translate", font=("Arial", 16), command=translateClick)
-translateButton.grid(column=2, row=2, padx=10, pady=10)
+translateButton = tk.Button(window, text="Translate", font=("Arial", 16), command=translateClick, bg="Green")
+translateButton.grid(column=2, row=3, padx=10, pady=10)
 
 
 
