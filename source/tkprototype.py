531a941057e2210afe4e36cc3f2eb8a654b709e9
Samueldnvn <116571481+Samueldnvn@users.noreply.github.com>
2023-07-21 12:53:56 -0400

    Added labels for input, output, and language selection

--- source/tkprototype.py
+++ source/tkprototype.py
@@ -31,19 +31,27 @@ def comboclick(event):
         test.translated_language = "fr"
 
 
+
+
 language_selection = ttk.Combobox(window, value=supported_languages, font=("Arial", 15), width=10)
 language_selection.current(0)
 language_selection.bind("<<ComboboxSelected>>", comboclick)
-language_selection.grid(column=2, row=1, padx=10, pady=10)
+language_selection.grid(column=2, row=2, padx=10, pady=10)
+language_selection_label = ttk.Label(window, text="Language:", font="Arial")
+language_selection_label.grid(column=2, row=1)
 
 inputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
-inputTextBox.grid(column=1, row=2, padx=10, pady=10)
+inputTextBox.grid(column=1, row=3, padx=10, pady=10)
+inputTextBox_label = ttk.Label(window, text="Input:", font="Arial")
+inputTextBox_label.grid(column=1, row=2)
 
 outputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
-outputTextBox.grid(column=3, row=2, padx=10, pady=10)
+outputTextBox.grid(column=3, row=3, padx=10, pady=10)
+outputTextBox_label = ttk.Label(window, text="Output:", font="Arial")
+outputTextBox_label.grid(column=3, row=2)
 
 translateButton = tk.Button(window, text="Translate", font=("Arial", 16), command=translateClick, bg="Green")
-translateButton.grid(column=2, row=3, padx=10, pady=10)
+translateButton.grid(column=2, row=4, padx=10, pady=10)
 
 
 
