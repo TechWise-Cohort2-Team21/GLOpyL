1ebe0fb1ac55fab121c9833ae80adfe980097e1f
LanielleP <nellysbookshelf@gmail.com>
2023-07-24 19:21:11 -0700

    modifications to GUI appearance part 1

--- source/test.py
+++ source/test.py
@@ -1,25 +1,7 @@
 from googletrans import Translator, constants
 from pprint import pprint
-import keywords
 
-
-#Keyword Dictionary: attempts to store correct keyword file in a dictionary
-# def make_keyword_dictionary(translated_language: str) -> dict:
-#     keyword_file_path = f"keywords_en2{translated_language}.rtf"
-#     try:
-#         with open(keyword_file_path, "r") as keyword_file:
-#             keywords = {}
-#             for line in keyword_file:
-#                 old, trans = line.strip().split(" ")
-#                 keywords[old] = trans
-#             return keywords
-#     except FileNotFoundError:
-#         print(f"Keyword file '{keyword_file_path}' not found.")
-#         exit(1)
-#     except Exception as e:
-#         print(f"Error occurred while reading the keyword file: {str(e)}")
-#         exit(1)
-
+translator = Translator()
 
 #Translates individual keyword or variable/function name
 def translate_word(word: str, lang: str, keywords: dict) -> str:
@@ -53,23 +35,6 @@ def translate_line(line: str, lang: str, keywords: dict, include_comments: bool 
     words.append(translate_word(current_word, lang, keywords))
     translated_line = "".join(words)
     return translated_line
-
-
-#Setup: select languages and create translator object
-#programming_language = "python"
-translated_language = "es"
-translator = Translator()
-
-#Applies translate functions to input file
-input = open("input.txt", "r")
-with open("output.rtf", "w") as output:
-    for line in input:
-        translation = translate_line(line, translated_language, keywords.es)
-        output.write(translation)
-input.close()
-output.close()
-# meow
-
 
 #Example of how to format/dissect output from a translation
 #print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
--- source/tkprototype.py
+++ source/tkprototype.py
@@ -2,26 +2,37 @@ import tkinter as tk
 from tkinter import ttk
 import keywords
 from test import translate_line
-from test import translated_language
 import codecs
 import pyperclip
 from tkinter import messagebox
 
+
+#Global variables
+translated_language = "es"
+programming_language = "python"
+current_keywords = keywords.es
+supported_languages = [
+    "Spanish Python",
+    "French Python"
+]
+
+
 # Creates the window
 window = tk.Tk()
 window.title("GLOpyL")
-window.geometry("900x500")
+#window.geometry("900x500")
 
-supported_languages = [
-    "Spanish",
-    "French"
-]
-currentkeywords = keywords.es
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window.geometry(f'{screen_width}x{screen_height}')
+window.minsize(900, 500)
+window.maxsize(screen_width, screen_height)
 
-# Variable to store the previous programming language
-previous_language = "python"
+
+# Setup for comments checkbox
 include_comments = tk.BooleanVar()
 include_comments.set(True)  # Set to True by default
+
 
 # Translates the input box, places in output box
 def translateClick():
@@ -29,54 +40,56 @@ def translateClick():
     input_text = inputTextBox.get("1.0", tk.END)
     output = ""
     for line in input_text.splitlines():
-        translation = translate_line(line, translated_language, currentkeywords, include_comments=include_comments.get())
+        translation = translate_line(line, translated_language, current_keywords, include_comments=include_comments.get())
         output += translation + "\n"
     outputTextBox.insert("1.0", output)
-    save_to_rtf(output)
 
-def save_to_rtf(output):
-    with codecs.open("output.rtf", "w", "utf-8") as output_file:
-        output_file.write(output)
-
+#to be used later for export purposes
+# def save_to_rtf(output):
+#     with codecs.open("output.rtf", "w", "utf-8") as output_file:
+#         output_file.write(output)
 
 def comboclick(event):
-    #global previous_language
-    global currentkeywords
-    if language_selection.get() == "Spanish":
+    global current_keywords
+    if language_selection.get() == "Spanish Python":
         translated_language = "es"
-        currentkeywords = keywords.es
-    elif language_selection.get() == "French":
+        current_keywords = keywords.es
+    elif language_selection.get() == "French Python":
         translated_language = "fr"
-        currentkeywords = keywords.fr
+        current_keywords = keywords.fr
 
-def copy_to_clipboard():
+def copy_input_to_clipboard():
+    original_code = inputTextBox.get("1.0", tk.END)
+    pyperclip.copy(original_code)
+    tk.messagebox.showinfo("Copy to Clipboard", "Original code has been copied to the clipboard.")
+
+def copy_output_to_clipboard():
     translated_code = outputTextBox.get("1.0", tk.END)
     pyperclip.copy(translated_code)
     tk.messagebox.showinfo("Copy to Clipboard", "Translated code has been copied to the clipboard.")
 
-
-language_selection = ttk.Combobox(window, value=supported_languages, font=("Arial", 15), width=10)
-language_selection.current(0)
-language_selection.bind("<<ComboboxSelected>>", comboclick)
-language_selection.grid(column=2, row=2, padx=10, pady=10)
-language_selection_label = ttk.Label(window, text="Language:", font="Arial")
-language_selection_label.grid(column=2, row=1)
+titleLabel = ttk.Label(window, text="GLOpyL", font=("Arial", 50)) #height=20, width=50, 
+titleLabel.grid(column=1, row=1, padx=100, pady=30)
 
 inputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
 inputTextBox.grid(column=1, row=3, padx=10, pady=10)
-inputTextBox_label = ttk.Label(window, text="Input:", font="Arial")
+inputTextBox_label = ttk.Label(window, text="English Python", font="Arial")
 inputTextBox_label.grid(column=1, row=2)
+copyInputButton = tk.Button(window, text="Copy", font=("Arial", 16), command=copy_input_to_clipboard)
+copyInputButton.grid(column=2, row=2, padx=10, pady=10)
 
+language_selection = ttk.Combobox(window, value=supported_languages, font=("Arial", 15), width=15)
+language_selection.current(0)
+language_selection.bind("<<ComboboxSelected>>", comboclick)
+# language_selection.grid(column=2, row=2, padx=10, pady=10)
+language_selection.grid(column=3, row=2)
 outputTextBox = tk.Text(window, height=10, width=30, font=("Arial", 16))
 outputTextBox.grid(column=3, row=3, padx=10, pady=10)
-outputTextBox_label = ttk.Label(window, text="Output:", font="Arial")
-outputTextBox_label.grid(column=3, row=2)
+copyOutputButton = tk.Button(window, text="Copy", font=("Arial", 16), command=copy_output_to_clipboard)
+copyOutputButton.grid(column=4, row=2, padx=10, pady=10)
 
 translateButton = tk.Button(window, text="Translate", font=("Arial", 16), command=translateClick, bg="Green")
 translateButton.grid(column=2, row=4, padx=10, pady=10)
-
-copyButton = tk.Button(window, text="Copy", font=("Arial", 16), command=copy_to_clipboard)
-copyButton.grid(column=2, row=5, padx=10, pady=10)
 
 commentsCheckbox = tk.Checkbutton(window, text="Include Comments", variable=include_comments, font=("Arial", 14))
 commentsCheckbox.grid(column=1, row=4, padx=10, pady=10, sticky="w")
