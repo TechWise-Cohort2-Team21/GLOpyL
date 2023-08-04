647cb6d0bbcdc2b758a72105a3b62914308770b4
Samueldnvn <116571481+Samueldnvn@users.noreply.github.com>
2023-08-04 11:16:41 -0400

    Format data for color coding

--- source/color_coded/code_translator.py
+++ source/color_coded/code_translator.py
@@ -8,7 +8,7 @@ translator = Translator()
 
 translation_memory = {}
 
-   
+
 
 def translate_word(word, lang, current_keywords):
     global length_incrementer
@@ -25,17 +25,26 @@ def translate_word(word, lang, current_keywords):
         length_incrementer += 1 + len(dictionary[word][0])
     else:
         words = word.split("_")
+#        if word == ' ':
+#            translated_word = [' ', 0, length_incrementer, length_incrementer + 1]
+#        else:
+#            translated_word = [translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"),
+#                                6,
+#                                (length_incrementer + 1),
+#                                (length_incrementer + 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_")))]
+#            length_incrementer += 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"))
         translated_word = [translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"),
                             6,
                             (length_incrementer + 1),
                             (length_incrementer + 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_")))]
         length_incrementer += 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"))
 
+
     return translated_word
 
 def translate_line(line: str, lang: str, current_keywords, include_comments: bool = True) -> str:
     global length_incrementer
-
+    length_incrementer = 0
     if not include_comments and line.lstrip().startswith("#"):
         return line
     ...
@@ -49,6 +58,8 @@ def translate_line(line: str, lang: str, current_keywords, include_comments: boo
     for char in line:
         if char.isalpha() or char == "_":
             current_word += char
+        #if char == " ":
+        #    words.append("a")
         elif current_word == "":
             words.append(char)
         else:
@@ -60,6 +71,21 @@ def translate_line(line: str, lang: str, current_keywords, include_comments: boo
     words.append(translate_word(current_word, lang, current_keywords))
     translated_line = []
     translated_line.append(words)
-    return translated_line
 
-length_incrementer = 0
+
+    for i in translated_line:
+        for j in range(len(i)):
+            if i[j] == ' ':
+                i[j] = [' ', 0, (i[j-1][3]), (i[j-1][3]) + 1]
+
+                for k in range(j, len(i)):
+                    if len(i[k]) > 2:
+                        if type(i[k]) == list:
+                            i[k][2] += 1
+                            i[k][3] += 1
+
+
+
+    
+
+    return translated_line
