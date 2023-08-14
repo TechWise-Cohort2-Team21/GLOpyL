80016379c8a32f60b28ed486f373256d7be9da6d
Samueldnvn <116571481+Samueldnvn@users.noreply.github.com>
2023-08-14 19:18:17 -0400

    color coded code_translator up to date

--- source/color_coded/code_translator.py
+++ source/color_coded/code_translator.py
@@ -1,38 +1,51 @@
-from googletrans import Translator, constants
-from pprint import pprint
+from googletrans import Translator
 import keywords
 
-
-
 translator = Translator()
-
 translation_memory = {}
 
+special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', ':', ',', '[', ']', '{', '}', '+', '=', '-', '<', '>', '/', '\\', '\t']
 
 
-def translate_word(word, lang, current_keywords):
+def translate_word(word: str, lang: str, preserve_keywords: bool = False) -> str:
     global length_incrementer
+    if lang not in translation_memory:
+        translation_memory[lang] = {}
+    if word in translation_memory[lang]:
+        return translation_memory[lang][word]
 
     if lang == "es":
         dictionary = keywords.es
     elif lang == "fr":
         dictionary = keywords.fr
+    elif lang == "zh":
+        dictionary = keywords.zh
+    elif lang == "hi":
+        dictionary = keywords.hi
     else:
-        dictionary = current_keywords
+        dictionary = {}
+
+    #if word in dictionary:
+    #    if preserve_keywords:
+    #        return word
+    #    return dictionary[word]
     
+    #translated_word = word
+    #words = word.split("_")
+    #try:
+    #    translated_word = translator.translate(" ".join(words), dest=lang).text.replace(" ", "_")
+    #except Exception as e:
+    #    print(f"Error translating word '{word}': {str(e)}")
+    #translation_memory[lang][word] = translated_word
+    #return translated_word
+
     if word in dictionary:
         translated_word = [dictionary[word][0], dictionary[word][1], (length_incrementer + 1), (length_incrementer + 1 + len(dictionary[word][0]))]
         length_incrementer += len(dictionary[word][0])
+    
+
     else:
         words = word.split("_")
-#        if word == ' ':
-#            translated_word = [' ', 0, length_incrementer, length_incrementer + 1]
-#        else:
-#            translated_word = [translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"),
-#                                6,
-#                                (length_incrementer + 1),
-#                                (length_incrementer + 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_")))]
-#            length_incrementer += 1 + len(translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"))
         translated_word = [translator.translate(" ".join(words), dest=lang).text.replace(" ", "_"),
                             6,
                             (length_incrementer + 1),
@@ -42,35 +55,60 @@ def translate_word(word, lang, current_keywords):
 
     return translated_word
 
-def translate_line(line: str, lang: str, current_keywords, include_comments: bool = True) -> str:
+
+def translate_line(line: str, lang: str, include_comments: bool = True, preserve_keywords: bool = False) -> str:
     global length_incrementer
     length_incrementer = 0
     if not include_comments and line.lstrip().startswith("#"):
-        return line
-    ...
+        return ""
 
-    ...
+    #result = []
+    #current_word = ""
 
+    #for char in line:
+    #    if char.isalpha() or char == "_":
+    #        current_word += char
+    #    elif current_word == "":
+    #        if char == "#" and not include_comments:
+    #            break
+    #        result.append(char)
+    #    else:
+    #        result.append(translate_word(current_word, lang, preserve_keywords))
+    #        current_word = ""
+    #        result.append(char)
+
+    #result.append(translate_word(current_word, lang, preserve_keywords))
+    #translated_line = "".join(result)
+    #return translated_line
 
     words = []
     current_word = ""
 
     for char in line:
-        if char.isalpha() or char == "_":
+        if char == '\t':
+            words.append([char, 0, length_incrementer, length_incrementer + 1])
+            length_incrementer += 1
+        elif char in special_char:
             current_word += char
-        #if char == " ":
-        #    words.append("a")
-        elif current_word == "":
+        elif char.isalpha() or char == "_":
+            current_word += char
+        elif current_word == "" and char not in special_char:
             words.append(char)
         else:
-            words.append(translate_word(current_word, lang, current_keywords))
+            words.append(translate_word(current_word, lang))
             current_word = ""
             words.append(char)
+
+    
                     
 
-    words.append(translate_word(current_word, lang, current_keywords))
+
+    
+    words.append(translate_word(current_word, lang))
     translated_line = []
     translated_line.append(words)
+    
+    
 
 
     for i in translated_line:
@@ -84,6 +122,8 @@ def translate_line(line: str, lang: str, current_keywords, include_comments: boo
                             i[k][2] += 1
                             i[k][3] += 1
 
+    
+
 
 
     
@@ -93,5 +133,4 @@ def translate_line(line: str, lang: str, current_keywords, include_comments: boo
 length_incrementer = 0
 
 
-print(translate_line("apple and pear and orange", "es", "es"))
-
+print(translate_line("this is a test sentence", "es"))
